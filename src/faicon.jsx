6e0fadf821b233534cf9b59a4bfb6e230eae0bb6
Max Zhuk <zhukmax@ya.ru>
2017-04-24 15:34:11 +0300

    Update faicon.jsx

--- src/faicon.jsx
+++ src/faicon.jsx
@@ -3,19 +3,62 @@ import 'font-awesome/css/font-awesome.css';
 
 class Faicon extends React.Component {
     render() {
-        let size,
-            name,
+        let size = "lg",
+            name = this.props.name,
+            animate = "",
+            border = "",
+            fixed= "",
+            flip = "",
+            rotate = "",
             nameOfClass;
 
-        name = this.props.name;
-
-        if (!this.props.size) {
-            size = "lg";
-        } else {
+        {/*
+          * Default size fa-lg
+         */}
+        if (this.props.size) {
             size = this.props.size;
         }
 
-        nameOfClass = "fa fa-" + name + " fa-" + size;
+        {/*
+          * Set animate for icon
+        */}
+        if (this.props.spin) {
+            animate = " fa-spin";
+        } else if (this.props.pulse) {
+            animate = " fa-pulse";
+        }
+
+        {/*
+          * Set border for icon
+        */}
+        if (this.props.border) {
+            border = " fa-border";
+        }
+
+        {/*
+         * Set fixed size for icon
+         */}
+        if (this.props.fixed) {
+            fixed = " fa-fw";
+        }
+
+        {/*
+         * Set flip for icon
+         */}
+        if (this.props.fh) {
+            flip = " fa-flip-horizontal";
+        } else if (this.props.fv) {
+            flip = " fa-flip-vertical";
+        }
+
+        {/*
+         * Set rotate for icon
+         */}
+        if (this.props.rotate) {
+            rotate = " fa-rotate-" + this.props.rotate;
+        }
+
+        nameOfClass = "fa fa-" + name + " fa-" + size + animate + border + fixed + flip;
         return (
             <i className={ nameOfClass }> </i>
         );
