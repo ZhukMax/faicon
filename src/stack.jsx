d8e4141d4b1c254121d75e2f5e320022cd8352db
Max Zhuk <zhukmax@ya.ru>
2017-04-26 00:09:46 +0300

    Update stack.jsx

--- src/stack.jsx
+++ src/stack.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import Faicon from './faicons.jsx';
+import Faicon from './faicon.jsx';
 
 class Stack extends React.Component {
     render() {
         let size = "lg",
             animate = "",
-            flip = "",
-            flip1 = "", flip2 = "",
-            rotate = "",
-            inverse = "",
+            fh1 = "", fv1 = "",
+            fh2 = "", fv2 = "",
+            rotate1 = "",
+            rotate2 = "",
+            inverse1 = false,
+            inverse2 = false,
             nameOfClass;
 
         {/*
@@ -31,25 +33,49 @@ class Stack extends React.Component {
          * Set flip for icon 1
          */}
         if (this.props.fh1) {
-            flip1 = "fh";
+            fh1 = true;
+            fv1 = false;
         } else if (this.props.fv1) {
-            flip1 = "fv";
+            fh1 = false;
+            fv1 = true;
         }
 
         {/*
          * Set flip for icon 2
          */}
         if (this.props.fh2) {
-            flip2 = "fh";
+            fh2 = true;
+            fv2 = false;
         } else if (this.props.fv2) {
-            flip2 = "fv";
+            fh2 = false;
+            fv2 = true;
+        }
+
+        {/*
+         * Set rotate for icons
+         */}
+        if (this.props.rotate1) {
+            rotate1 = this.props.rotate1;
+        }
+        if (this.props.rotate2) {
+            rotate2 = this.props.rotate2;
+        }
+
+        {/*
+         * Set inverse colors for icons
+         */}
+        if (this.props.inverse1) {
+            inverse1 = true;
+        }
+        if (this.props.inverse2) {
+            inverse2 = true;
         }
 
         nameOfClass = "fa-stack fa-" + size + animate;
         return (
             <span className={ nameOfClass }>
-                <Faicon name={ this.props.name1 } stack="1" size="none" { flip1 } fh/>
-                <Faicon name={ this.props.name2 } stack="2" size="none" { flip2 }/>
+                <Faicon name={ this.props.name1 } stack="1" size="none" fh={ fh1 } fv={ fv1 } rotate={ rotate1 } inverse={ inverse1 }/>
+                <Faicon name={ this.props.name2 } stack="2" size="none" fh={ fh2 } fv={ fv2 } rotate={ rotate2 } inverse={ inverse2 }/>
             </span>
         );
     }
